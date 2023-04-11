1f0bb2e869123bfdcdd82c8b60d8230f6e820b0b
Olekdsandr Babilia <oleksandrbabilia@gmail.com>
2023-04-11 13:06:42 +0300

    Fixed: Admin migration bug

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'core',
+    'core.user'
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core_user.User'
