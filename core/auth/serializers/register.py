8e2b4c7d0a807401121766d727ee82bb8a7fc51d
Olekdsandr Babilia <oleksandrbabilia@gmail.com>
2023-04-12 00:34:22 +0300

    Added: Register serializer

---
+++ core/auth/serializers/register.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+
+from core.user.serializers import UserSerializer
+from core.user.models import User
+
+
+class RegisterSerializer(UserSerializer):
+    """
+        Registration serializer for requests and user creation
+    """
+    password = serializers.CharField(
+        max_length=128,
+        min_length=8,
+        write_only=True,
+        required=True
+    )
+    class Meta:
+        model = User
+        fields =  ['id', 'bio', 'avatar', 'email',
+            'username', 'first_name', 'last_name',
+            'password']
+        
+    def create(self, validated_data):
+        return User.objects.create_user(validated_data)
