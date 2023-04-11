4099e179414a230418450236d3ecd56a69cc9854
Olekdsandr Babilia <oleksandrbabilia@gmail.com>
2023-04-11 13:32:55 +0300

    Added: User serializer

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    'rest_framework',
+    
     'core',
-    'core.user'
+    'core.user',
 ]
 
 MIDDLEWARE = [
