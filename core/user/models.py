ebd339178ba28c09b39048457c5dd7318158f367
Olekdsandr Babilia <oleksandrbabilia@gmail.com>
2023-04-11 12:53:45 +0300

    Added: User manager

--- core/user/models.py
+++ core/user/models.py
@@ -10,6 +10,48 @@ from django.db import models
 from django.http import Http404
 
 
+class UserManager(BaseUserManager):
+    def get_object_by_public_id(self, public_id):
+        try:
+            instance = self.get(public_id=public_id)
+            return instance
+        except (ObjectDoesNotExist, ValueError, TypeError):
+            return Http404
+        
+    def create_user(self, username, email, password=None, **kwargs):
+        """Create and return a `User` with an email, phone
+            number, username and password."""
+        if username is None:
+            raise TypeError('Users must have a username.')
+        if email is None:
+            raise TypeError('Users must have an email.')
+        if password is None:
+            raise TypeError('Users must have an password.')
+        
+        user = self.model(username=username, email=self.normalize_email(email), **kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+        
+        return user
+    
+    def create_superuser(self, username, email, password=None, **kwargs):
+        """Create and return a `User` with superuser (admin)
+            permissions."""
+        if username is None:
+            raise TypeError('Superusers must have a username.')
+        if email is None:
+            raise TypeError('Superusers must have an email.')
+        if password is None:
+            raise TypeError('Superusers must have an password.')
+        
+        user = self.create_user(username, email, password, **kwargs)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        
+        return user
+
+
 class User(AbstractBaseUser, PermissionsMixin):
     public_id = models.UUIDField(
         db_index=True,
@@ -37,3 +79,4 @@ class User(AbstractBaseUser, PermissionsMixin):
     @property
     def name(self):
         return f"{self.first_name} {self.last_name}"
+    
