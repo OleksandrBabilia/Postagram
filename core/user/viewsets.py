e189d1adf6910e8dba2e1a7f2edf287a2dd1bfe9
Olekdsandr Babilia <oleksandrbabilia@gmail.com>
2023-04-12 15:58:38 +0300

    Fixed: User ViewSet to use Abstract ViewSet

--- core/user/viewsets.py
+++ core/user/viewsets.py
@@ -3,9 +3,9 @@ from rest_framework import viewsets
 
 from .serializers import UserSerializer
 from .models import User
+from core.abstract.viewsets import AbstractViewSet
 
-
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(AbstractViewSet):
     http_method_names = ('patch', 'get',)
     permission_classes = (IsAuthenticated, )
     serializer_class = UserSerializer
