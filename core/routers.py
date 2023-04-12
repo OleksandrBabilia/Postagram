98fcbaae11994f252e632dfd10584c9922eade57
Olekdsandr Babilia <oleksandrbabilia@gmail.com>
2023-04-12 13:26:08 +0300

    Added: Refresh viewset and router

--- core/routers.py
+++ core/routers.py
@@ -1,12 +1,14 @@
 from rest_framework import routers
 
 from .user.viewsets import UserViewSet
-from core.auth.viewsets import RegisterViewSet
+from core.auth.viewsets import RegisterViewSet, LoginViewSet, RefreshViewSet
 
 router = routers.SimpleRouter()
 
 router.register(r'users', UserViewSet, basename='user')
 router.register(r'auth/register', RegisterViewSet, basename='auth-register')
+router.register(r'auth/login', LoginViewSet, basename='auth-login')
+router.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
 
 urlpatterns = [
     *router.urls,
