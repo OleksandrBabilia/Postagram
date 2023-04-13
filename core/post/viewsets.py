93270f48233ae68db6f73b8ef3ec72dc38c73866
Olekdsandr Babilia <oleksandrbabilia@gmail.com>
2023-04-13 13:27:01 +0300

    Added: Like and Romove Like Method to Post ViewSerializer

--- core/post/viewsets.py
+++ core/post/viewsets.py
@@ -1,5 +1,6 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from rest_framework import status 
 
 from core.abstract.viewsets import AbstractViewSet
@@ -26,3 +27,22 @@ class PostViewSet(AbstractViewSet):
         self.perform_create(serializer)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     
+    @action(method=['post'], detail=True)
+    def like (self, request, *args, **kwargs):
+        post = self.get_object()
+        user = self.request.user
+        
+        user.like(post)
+        
+        serializer = self.serializer_class(post)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(method=['post'], detail=True)
+    def remove_like (self, request, *args, **kwargs):
+        post = self.get_object()
+        user = self.request.user
+        
+        user.remove_like(post)
+        
+        serializer = self.serializer_class(post)
+        return Response(serializer.data, status=status.HTTP_200_OK)           
