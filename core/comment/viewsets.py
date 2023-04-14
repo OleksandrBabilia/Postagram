512c948d1acbd8e07750c6143a07fcde299765b8
Olekdsandr Babilia <oleksandrbabilia@gmail.com>
2023-04-14 19:42:48 +0300

    Added: Comment ViewSet

---
+++ core/comment/viewsets.py
@@ -0,0 +1,47 @@
+from django.http.response import Http404
+
+from rest_framework.response import Response
+from rest_framework import status
+
+from core.abstract.viewsets import AbstractViewSet
+from core.post.permissions import UserPermission
+from .models import Comment
+from .serializers import CommentSerializer
+
+
+class CommentViewSet(AbstractViewSet):
+    http_method_names = ('post', 'get', 'put', 'delete')
+    permission_classes = (UserPermission, )
+    serializer_class = CommentSerializer
+    
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return Comment.objects.all()
+        
+        post_pk = self.kwargs['post_pk']
+        if post_pk is None:
+            return Http404
+        queryset = Comment.objects.filter(post__public_id=post_pk)
+        
+        return queryset
+    
+    def get_object(self):
+        obj = Comment.objects.get_object_by_public_id(self.kwargs['pk'])
+        self.check_object_permissions(self.request, obj)
+        
+        return obj
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_constraint=True)
+        self.perform_create(serializer)
+        
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    def validate_post(self, value):
+        if self.instance:
+            return self.instance.post
+        
+        return value        
+    
+    
