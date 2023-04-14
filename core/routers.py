1895aef12b212ca050e057cf5ccd7dbc134e713a
Olekdsandr Babilia <oleksandrbabilia@gmail.com>
2023-04-14 19:46:19 +0300

    Added: Nested Commnet routers

--- core/routers.py
+++ core/routers.py
@@ -1,8 +1,9 @@
-from rest_framework import routers
+from rest_framework_nested import routers
 
 from .user.viewsets import UserViewSet
 from core.auth.viewsets import RegisterViewSet, LoginViewSet, RefreshViewSet
 from core.post.viewsets import PostViewSet
+from core.comment.viewsets import CommentViewSet
 
 router = routers.SimpleRouter()
 
@@ -10,8 +11,12 @@ router.register(r'users', UserViewSet, basename='user')
 router.register(r'auth/register', RegisterViewSet, basename='auth-register')
 router.register(r'auth/login', LoginViewSet, basename='auth-login')
 router.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
-router.register(r'posts', PostViewSet, basename='post')
+router.register(r'post', PostViewSet, basename='post')
+
+posts_router = routers.NestedSimpleRouter(router, r'post', lookup='post')
+posts_router.register(r'comment', CommentViewSet, basename='post-comment')
 
 urlpatterns = [
     *router.urls,
+    *posts_router.urls,
 ]
