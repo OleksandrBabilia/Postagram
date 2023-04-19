f7e500ef9cc2e547c3872bb1b348d1b9edfb7330
Olekdsandr Babilia <oleksandrbabilia@gmail.com>
2023-04-19 23:31:26 +0300

    Updated: Home Page

--- social-media-app/src/pages/Home.jsx
+++ social-media-app/src/pages/Home.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+import { getUser } from "../hooks/user.actions";
 
 function Home() {
-    return (
-        <div>
-            <h1>Profile!</h1>
-            <p>
-                Welcome!
-            </p>
-        </div>
-    )
+  const user = getUser();
+
+  if (!user) {
+    return <div>Loading!</div>;
+  }
+
+  return (
+    <div>
+      <h1>Profile</h1>
+      <p>Welcome!</p>
+    </div>
+  );
 }
 
 export default Home;
