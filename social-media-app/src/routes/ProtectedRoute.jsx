b09b4c688f9b411094e7186905b9e4bf44578b46
Olekdsandr Babilia <oleksandrbabilia@gmail.com>
2023-04-19 22:48:19 +0300

    Added: Protected Route

---
+++ social-media-app/src/routes/ProtectedRoute.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+
+function ProtectedRoute({ children }) {
+    const { user } = JSON.parse(localStorage.getItem("auth"));
+    return user.account ? <>{children}</> : <Navigate to="/login/" />;
+}
+
+export default ProtectedRoute;
