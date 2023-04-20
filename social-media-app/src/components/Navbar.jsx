ca0e48e837d6d8c3504dbb1d426f337586f6eba1
Olekdsandr Babilia <oleksandrbabilia@gmail.com>
2023-04-20 12:50:28 +0300

    Updated: Navbar to user Profile

--- social-media-app/src/components/Navbar.jsx
+++ social-media-app/src/components/Navbar.jsx
@@ -1,36 +1,33 @@
 import React from "react";
-import { randomAvatar } from "../utils";
 
 import { Navbar, Container, Image, NavDropdown, Nav } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { getUser, useUserActions } from "../hooks/user.actions";
 
-function Navigationbar() {
-  const navigate = useNavigate();
-  const handleLogout = () => {
-    localStorage.removeItem("auth");
-    navigate("/login/");
-  };
+function NavigationBar() {
+  const userActions = useUserActions();
+
+  const user = getUser();
 
   return (
     <Navbar bg="primary" variant="dark">
       <Container>
-        <Navbar.Brand className="fw-bold" href="#home">
+        <Navbar.Brand className="fw-bold" as={Link} to={`/`}>
           Postagram
         </Navbar.Brand>
         <Navbar.Collapse className="justify-content-end">
           <Nav>
             <NavDropdown
               title={
-                <Image
-                  src={randomAvatar()}
-                  roundedCircle
-                  width={36}
-                  height={36}
-                />
+                <Image src={user.avatar} roundedCircle width={36} height={36} />
               }
             >
-              <NavDropdown.Item href="#">Profile</NavDropdown.Item>
-              <NavDropdown.Item onClick={handleLogout}>Logout</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to={`/profile/${user.id}/`}>
+                Profile
+              </NavDropdown.Item>
+              <NavDropdown.Item onClick={userActions.logout}>
+                Logout
+              </NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
@@ -39,4 +36,4 @@ function Navigationbar() {
   );
 }
 
-export default Navigationbar;
+export default NavigationBar;
