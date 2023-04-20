15346cb6f7bf21a98d2d6c51b2c427279fcb9e4c
Olekdsandr Babilia <oleksandrbabilia@gmail.com>
2023-04-20 12:28:04 +0300

    Added: Update Comment Component

---
+++ social-media-app/src/components/comments/UpdateComment.jsx
@@ -0,0 +1,91 @@
+import React, { useState, useContext } from "react";
+import { Button, Modal, Form, Dropdown } from "react-bootstrap";
+import axiosService from "../../helpers/axios";
+
+import { Context } from "../Layout";
+
+function UpdateComment(props) {
+  const { postId, comment, refresh } = props;
+  const [show, setShow] = useState(false);
+  const [validated, setValidated] = useState(false);
+  const [form, setForm] = useState({
+    author: comment.author.id,
+    body: comment.body,
+    post: postId,
+  });
+
+  const { setToaster } = useContext(Context);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const updateCommentForm = event.currentTarget;
+
+    if (updateCommentForm.checkValidity() === false) {
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+
+    const data = {
+      author: form.author,
+      body: form.body,
+      post: postId,
+    };
+
+    axiosService
+      .put(`/post/${postId}/comment/${comment.id}/`, data)
+      .then(() => {
+        handleClose();
+        setToaster({
+          type: "success",
+          message: "Comment updated 🚀",
+          show: true,
+          title: "Success!",
+        });
+        refresh();
+      })
+      .catch(() => {
+        setToaster({
+          type: "danger",
+          message: "An error occurred.",
+          show: true,
+          title: "Comment Error",
+        });
+      });
+  };
+
+  return (
+    <>
+      <Dropdown.Item onClick={handleShow}>Modify</Dropdown.Item>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton className="border-0">
+          <Modal.Title>Update Post</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="border-0">
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Form.Group className="mb-3">
+              <Form.Control
+                name="body"
+                value={form.body}
+                onChange={(e) => setForm({ ...form, body: e.target.value })}
+                as="textarea"
+                rows={3}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleSubmit}>
+            Modify
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default UpdateComment;
