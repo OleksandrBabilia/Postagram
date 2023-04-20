72dceb6336295e42719629200050382bbf04ca2d
Olekdsandr Babilia <oleksandrbabilia@gmail.com>
2023-04-20 12:30:31 +0300

    Updated: App with new Routes

--- social-media-app/src/App.js
+++ social-media-app/src/App.js
@@ -4,15 +4,27 @@ import ProtectedRoute from "./routes/ProtectedRoute";
 import Home from "./pages/Home";
 import Registration from "./pages/Registration";
 import Login from "./pages/Login";
+import SinglePost from "./pages/SinglePost";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={
-        <ProtectedRoute>
-          <Home />
-        </ProtectedRoute>
-      } />
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/post/:postId/"
+        element={
+          <ProtectedRoute>
+            <SinglePost />
+          </ProtectedRoute>
+        }
+      />
       <Route path="/login/" element={<Login />} />
       <Route path="/register/" element={<Registration />} />
     </Routes>
