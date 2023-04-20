637595615b3ddb0d93ec17de751c9f0d04e34f09
Olekdsandr Babilia <oleksandrbabilia@gmail.com>
2023-04-20 12:33:04 +0300

    Updated: Home Page to use Profile Card

--- social-media-app/src/pages/Home.jsx
+++ social-media-app/src/pages/Home.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import Layout from "../components/Layout";
 import { Row, Col, Image } from "react-bootstrap";
-import { randomAvatar } from "../utils";
 import useSWR from "swr";
 import { fetcher } from "../helpers/axios";
 import { getUser } from "../hooks/user.actions";
 import { Post } from "../components/posts";
 import CreatePost from "../components/posts/CreatePost";
+import ProfileCard from "../components/profile/ProfileCard";
 
 function Home() {
   const posts = useSWR("/post/", fetcher, {
     refreshInterval: 20000,
   });
+  const profiles = useSWR("/user/?limit=5", fetcher);
 
   const user = getUser();
 
@@ -26,7 +27,7 @@ function Home() {
           <Row className="border rounded  align-items-center">
             <Col className="flex-shrink-1">
               <Image
-                src={randomAvatar()}
+                src={user.avatar}
                 roundedCircle
                 width={52}
                 height={52}
@@ -42,6 +43,15 @@ function Home() {
               <Post key={index} post={post} refresh={posts.mutate} />
             ))}
           </Row>
+        </Col>
+        <Col sm={3} className="border rounded py-4 h-50">
+          <h4 className="font-weight-bold text-center">Suggested people</h4>
+          <div className="d-flex flex-column">
+            {profiles.data &&
+              profiles.data.results.map((profile, index) => (
+                <ProfileCard key={index} user={profile} />
+              ))}
+          </div>
         </Col>
       </Row>
     </Layout>
