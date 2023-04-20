008f39cdf16408731e30afc1b94c9902318c2bde
Olekdsandr Babilia <oleksandrbabilia@gmail.com>
2023-04-20 12:42:19 +0300

    Added: User Serializer to_representation method

--- core/user/serializers.py
+++ core/user/serializers.py
@@ -2,8 +2,18 @@ from rest_framework import serializers
 
 from .models import User
 from core.abstract.serializers import AbstractSerializer
-
+from django.conf import settings
 class UserSerializer(AbstractSerializer):
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        if not representation['avatar']:
+            representation['avatar'] = settings.DEFAULT_AVATAR_URL
+            return representation
+        if settings.DEBUG:
+            request = self.context.get('request')
+            representation['avatar'] = request.build_absolute_uri(representation['avatar'])
+        return representation
+    
     class Meta:
         model = User
         fields =['id', 'username', 'first_name',
