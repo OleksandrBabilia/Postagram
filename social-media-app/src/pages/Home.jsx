8bd86d148bdffdedfc845164b4f1ef3fd4c372ea
Olekdsandr Babilia <oleksandrbabilia@gmail.com>
2023-04-20 12:00:59 +0300

    Added: Layout and Changed NavBar directory

--- social-media-app/src/pages/Home.jsx
+++ social-media-app/src/pages/Home.jsx
@@ -1,19 +1,10 @@
 import React from "react";
-import { getUser } from "../hooks/user.actions";
+import Layout from "../components/Layout";
 
 function Home() {
-  const user = getUser();
-
-  if (!user) {
-    return <div>Loading!</div>;
-  }
-
   return (
-    <div>
-      <h1>Profile</h1>
-      <p>Welcome!</p>
-    </div>
+  <Layout>
+  </Layout>
   );
-}
-
-export default Home;
+ }
+ export default Home;
