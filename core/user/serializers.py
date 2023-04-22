09b31de3f3206836b1e56c6bbba0dbe357cfdb8a
Olekdsandr Babilia <oleksandrbabilia@gmail.com>
2023-04-22 20:04:35 +0300

    Added: Post Count tp User Serializer

--- core/user/serializers.py
+++ core/user/serializers.py
@@ -4,6 +4,11 @@ from .models import User
 from core.abstract.serializers import AbstractSerializer
 from django.conf import settings
 class UserSerializer(AbstractSerializer):
+    posts_count = serializers.SerializerMethodField()
+    
+    def get_posts_count(self, instance):
+        return instance.post_set.all().count()
+    
     def to_representation(self, instance):
         representation = super().to_representation(instance)
         if not representation['avatar']:
@@ -18,5 +23,5 @@ class UserSerializer(AbstractSerializer):
         model = User
         fields =['id', 'username', 'first_name',
             'last_name', 'bio', 'avatar', 'email',
-            'is_active', 'created', 'updated']
+            'is_active', 'created', 'updated', 'posts_count']
         read_only_field = ['is_active']
