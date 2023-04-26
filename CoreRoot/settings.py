50b8e589e6433fc93791dc063bfa082a6c11021b
Olekdsandr Babilia <oleksandrbabilia@gmail.com>
2023-04-27 00:04:18 +0300

    Fixed: Redis url

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -168,7 +168,7 @@ DEFAULT_AVATAR_URL = "https://avatars.dicebear.com/api/identicon/.svg"
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://redis:6379",
+        "LOCATION": "redis://localhost:6379",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         },
